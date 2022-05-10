f5a4b3a8f08c663bd6bd6df02a15435574e5db08
adyaman <adnan.yaman@kantox.com>
2022-05-10 14:26:09 +0200

    Article_params made private

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,8 @@ class ArticlesController < ApplicationController
     flash[:success] = "Article has been created"
     redirect_to articles_path
   end
+
+  private
   def article_params
     params.require(:article).permit(:title, :body)
   end
