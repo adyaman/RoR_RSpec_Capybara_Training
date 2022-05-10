f03d5cfbc2628b2c766788231dac8cfdca686e91
adyaman <adnan.yaman@kantox.com>
2022-05-10 13:13:10 +0200

    Articles controller create function added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,10 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def create
+    @article = Article.new(@article_param)
+    @article.save
+    flash[:success] = "Article has been created"
+    redirect_to articles_path
+  end
 end
