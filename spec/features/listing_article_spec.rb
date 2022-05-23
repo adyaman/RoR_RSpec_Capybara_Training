a1f69974018b72a541ac6ddcf125d20f3fd6e46f
adyaman <adnan.yaman@kantox.com>
2022-05-23 17:39:48 +0200

    No articles listed -unsuccessful

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -17,5 +17,21 @@ RSpec.feature "Listing articles" do
     expect(page).to have_link(@article2.title)
 
   end
+  scenario "No articles available" do
+    Article.delete_all
+    visit "/"
+
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+
+    within("h1#no-articles") do
+      expect(page).to have_content("No articles created yet")
+    end
+
+  end
 
 end
